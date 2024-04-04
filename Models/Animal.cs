2e08e8e690c69a783d82752a69520dd8d158c3fe
Askarbekov Aktan <akn392204@gmail.com>
2024-04-04 17:34:07 +0600

    added model of Animal and nuget packages

---
+++ Models/Animal.cs
@@ -0,0 +1,12 @@
+namespace HomeWork49.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string KindOfAnimal { get; set; }
+    public string? Notes { get; set; }
+    public int Price { get; set; }
+    public string Avatar { get; set; }
+}
